ee13229845d2b3e5c9a81441b2e3e1a4385bb5ab
Christian Jonak <christian@jonak.org>
2019-10-14 13:59:50 +0200

    Make gunicorn bind & workers configureable

--- netbox/files/gunicorn_config.py
+++ netbox/files/gunicorn_config.py
@@ -1,6 +1,6 @@
 {% from "netbox/map.jinja" import netbox with context %}
 command = '{{ netbox.service.homedir }}/bin/gunicorn'
 pythonpath = '{{ netbox.service.homedir }}/bin/'
-bind = '127.0.0.1:8001'
-workers = 3
+bind = '{{ netbox.service.gunicorn.bind }}'
+workers = {{ netbox.service.gunicorn.workers }}
 user = '{{ netbox.service.user }}'
